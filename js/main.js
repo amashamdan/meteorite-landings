74f48561f45833aa5742d938b620b298c13a5639
Amer Hamdan <amashamdan@gmail.com>
2016-09-14 16:24:56 -0700

    Bug: Fixed a bug where smaller circles couldn't be hovered.
    Is some areas with high meteorite density, some circles where covered by larger circles.
    The code didn't detect these small covered circles when they were hovered.
    This happened bacause larger circles were drawn after smaller circles.
    Solution was to draw larger circles first and then smaller circles.
    d3 sort funtion was used to draw the circles in an order based on the meteorites' mass.
    Larger circles are drawn first and then smaller ones and so on.

--- js/main.js
+++ js/main.js
@@ -90,58 +90,10 @@ function plotPoints() {
 			.attr("stroke", "black")
 			.attr("stroke-width", 0.5)
 			.style("opacity", 0.7)
-/*
-		for (var point in json.features) {
-			if (json.features[point].geometry) {
-				var lon = json.features[point].geometry.coordinates[0];
-				var lat = json.features[point].geometry.coordinates[1];
-				var convertedLocation = projection([lon, lat]);
-				chart.append("circle")
-					.attr("cx", convertedLocation[0])
-					.attr("cy", convertedLocation[1])
-					.attr("r", function() {
-						return massScale(Number(json.features[point].properties.mass))
-					})
-					.attr("class", "meteorite")
-					.attr("name", function() {
-						return json.features[point].properties.name
-					})
-					.attr("mass", function() {
-						return json.features[point].properties.mass
-					})
-					.attr("classification", function() {
-						return json.features[point].properties.recclass
-					})
-					.attr("lat", function() {
-						return json.features[point].properties.reclat
-					})
-					.attr("lon", function() {
-						return json.features[point].properties.reclong
-					})
-					.attr("year", function() {
-						if (json.features[point].properties.year) {
-							var year = json.features[point].properties.year
-							year = year.split("-")[0];
-							return year
-						} else {
-							return "Unknown";
-						}
-					})
-					.attr("fill", function() {
-						//quantize not used because most meteorites are small, no variety in colors.
-						return colors[Math.floor(Math.random() * 8)];
-					})
-					.attr("stroke", "black")
-					.attr("stroke-width", 0.5)
-					.style("opacity", 0.7)
-			}
-		}
 
-*/
-		/*d3.selectAll("circle").sort(function(a, b) {
-			console.log(a.mass);
-			return d3.ascending(a.properties.mass, b.properties.mass);
-		})*/
+		d3.selectAll("circle").sort(function(a, b) {
+			return b.properties.mass - a.properties.mass;
+		})
 
 		var tooltipStatus = false;
 		$(".meteorite").hover(function(e) {
