c1cb4220a9e7e889c9032fb4d600d6f4a1671f16
Amer Hamdan <amashamdan@gmail.com>
2016-09-13 17:59:59 -0700

    Feat: Meteorites now added to the chart.
    Locations of meteorite landings on earth are now added to the chart.
    Function plotPoints was added to handle that.
    The function is called from within d3.json which drew the earth to ensure that the points are drawn over earth.
    Changed the color of earth from random coloring to #A3CCFF.
    Circles representing meteorite landing have random colors from a set of 8 colors.
    The size of each circle depends on the mass of each meteorite it represents.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var width = $(window).width();
 var height = width / 2;
-var colors = ["#8dd3c7", "#ffffb3", "#bebada", "#fb8072", "#80b1d3", "#fdb462", "#b3de69", "#fccde5", "#d9d9d9"];
+var colors = ["#d53e4f", "#f46d43", "#fdae61", "#fee08b", "#e6f598", "#abdda4", "#66c2a5", "#3288bd"];
 var chart = d3.select("#chart-area").append("svg")
 				.attr("width", width)
 				.attr("height", height)
-				.style("background-color", "658EF3");
+				.style("background-color", "#A3CCFF");
 
 var projection = d3.geo.equirectangular().translate([width/2, height/2]).scale([width / 6.315789]);
 
@@ -18,9 +18,43 @@ d3.json("js/earth.json", function(json) {
 		.append("path")
 		.attr("d", path) //d (the path data attribute) is referred to our path generator,which magically takes the bound geodata and calculates all that crazy SVG code.
 		.style("fill", function() {
-			return colors[Math.floor(Math.random() * 9)]
+			return '#F3F1EE'
 		})
 		.attr("stroke", "white")
 		.attr("stroke-width", 1)
+	plotPoints();
 })
 
+function plotPoints() {
+	d3.json("js/meteorite-strike-data.json", function(json) {
+		var massScale = d3.scale.sqrt()
+							.domain([d3.min(json.features, function(d) {
+								return Number(d.properties.mass);
+							}), d3.max(json.features, function(d) {
+								if (Number(d.properties.mass) > 500000) {
+									console.log(Number(d.properties.mass))
+								}
+								return Number(d.properties.mass);
+							})])
+							.range([2, 60])
+
+		for (var point in json.features) {
+			if (json.features[point].geometry) {
+				var lon = json.features[point].geometry.coordinates[0];
+				var lat = json.features[point].geometry.coordinates[1];
+				var convertedLocation = projection([lon, lat]);
+				chart.append("circle")
+					.attr("cx", convertedLocation[0])
+					.attr("cy", convertedLocation[1])
+					.attr("r", function() {
+						return massScale(Number(json.features[point].properties.mass))
+					})
+					.attr("fill", function() {
+						//quantize not used because most meteorites are small, no variety in colors.
+						return colors[Math.floor(Math.random() * 8)];
+					})
+					.style("opacity", 0.7);
+			}
+		}
+	})
+}
